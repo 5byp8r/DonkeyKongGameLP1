1b31fe828d47149de748306b8377aea6f0e69f23
Daniel-Henriq <137714763+Daniel-Henriq@users.noreply.github.com>
2024-08-25 10:27:26 -0300

    posição das plataformas

--- DonkeyKong/main.cpp
+++ DonkeyKong/main.cpp
@@ -15,6 +15,39 @@ int main(int argc, char **argv) {
 	window.setFramerateLimit(60);
 	window.setVerticalSyncEnabled(true);
 
+	sf::RectangleShape plataforma[7];
+	plataforma[0].setSize(sf::Vector2f(680, 15));
+	plataforma[0].setFillColor(sf::Color::Red);
+	plataforma[0].setPosition(50,570);
+
+	plataforma[1].setSize(sf::Vector2f(630, 15));
+	plataforma[1].setFillColor(sf::Color::Cyan);
+	plataforma[1].setPosition(50,470);
+	plataforma[1].setRotation(2.f);
+
+	plataforma[2].setSize(sf::Vector2f(630, 15));
+	plataforma[2].setFillColor(sf::Color::Cyan);
+	plataforma[2].setPosition(720,390);
+	plataforma[2].setRotation(178.f);
+
+	plataforma[3].setSize(sf::Vector2f(630, 15));
+	plataforma[3].setFillColor(sf::Color::Cyan);
+	plataforma[3].setPosition(50,290);
+	plataforma[3].setRotation(2.f);
+
+	plataforma[4].setSize(sf::Vector2f(630, 15));
+	plataforma[4].setFillColor(sf::Color::Cyan);
+	plataforma[4].setPosition(720,210);
+	plataforma[4].setRotation(178.f);
+
+	plataforma[5].setSize(sf::Vector2f(620, 15));
+	plataforma[5].setFillColor(sf::Color::Red);
+	plataforma[5].setPosition(50,130);
+
+	plataforma[6].setSize(sf::Vector2f(90, 15));
+	plataforma[6].setFillColor(sf::Color::White);
+	plataforma[6].setPosition(180,60);
+
 	sf::Texture marioTexture;
 	sf::Sprite marioSprite;
 
@@ -28,8 +61,8 @@ int main(int argc, char **argv) {
 	marioSprite.setTexture(marioTexture);
 
 	marioSprite.setOrigin(marioSprite.getLocalBounds().width/2., marioSprite.getLocalBounds().height/2.);
-	marioSprite.setPosition(400,300);
-	marioSprite.setScale(0.07, 0.07);
+	marioSprite.setPosition(130,551);
+	marioSprite.setScale(0.065, 0.065);
 
 	while(window.isOpen()){
 		sf::Event event;
@@ -53,6 +86,12 @@ int main(int argc, char **argv) {
 
 		window.clear(sf::Color::Black);
 		window.draw(marioSprite);
+		window.draw(marioSprite);
+		int NumPlataforma=0;
+		while(NumPlataforma<7){
+			window.draw(plataforma[NumPlataforma]);
+			NumPlataforma++;
+		};
 		window.display();
 		sf::sleep(sf::milliseconds(10.0f));
 	}
