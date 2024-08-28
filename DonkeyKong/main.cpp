b6abae8929ed43932aef761b702e618cc1bcaafb
André Victor Gonçalves Nascimento <andrevictor20011@gmail.com>
2024-08-27 21:48:51 -0300

    Player class created

--- DonkeyKong/main.cpp
+++ DonkeyKong/main.cpp
@@ -8,114 +8,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/audio.hpp>
-#include <ctime>
-
-/* Antiga função
-void createPlatforms(sf::RectangleShape *plataforma){
-	int contadorChaoPosition = 0;
-	int contadorChaoCriados = 0;
-	int floorRotation = 0;
-
-	for(int i = 0; i < 27; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(0 + contadorChaoPosition,570);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-	}
-
-	contadorChaoPosition = 0;
-
-	for(int i = contadorChaoCriados + 1; i < 50; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(50 + contadorChaoPosition, 480 + floorRotation);
-		plataforma[i].setRotation(2.f);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-		floorRotation++;
-	}
-
-	contadorChaoPosition = 0;
-	floorRotation = 0;
-
-	for(int i = contadorChaoCriados + 1; i < 72; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(80 + contadorChaoPosition, 420 - floorRotation);
-		plataforma[i].setRotation(-1.f);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-		floorRotation++;
-	}
-
-	contadorChaoPosition = 0;
-	floorRotation = 0;
-
-	for(int i = contadorChaoCriados + 1; i < 94; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(50 + contadorChaoPosition, 320 + floorRotation);
-		plataforma[i].setRotation(1.f);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-		floorRotation++;
-	}
-
-	contadorChaoPosition = 0;
-	floorRotation = 0;
-
-	for(int i = contadorChaoCriados + 1; i < 116; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(80 + contadorChaoPosition, 260 - floorRotation);
-		plataforma[i].setRotation(-1.f);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-		floorRotation++;
-	}
-
-	contadorChaoPosition = 0;
-	floorRotation = 0;
-
-	for(int i = contadorChaoCriados + 1; i < 126; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(680 - contadorChaoPosition, 185 - floorRotation);
-		plataforma[i].setRotation(0.5f);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-		floorRotation++;
-	}
-
-	for(int i = contadorChaoCriados + 1; i < 139; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(680 - contadorChaoPosition, 185 - floorRotation);
-
-		contadorChaoPosition += 30;
-		contadorChaoCriados++;
-	}
-
-	contadorChaoPosition = 0;
-	floorRotation = 0;
-
-	for(int i = contadorChaoCriados; i < 142; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(285 + contadorChaoPosition, 100);
-
-		contadorChaoPosition += 30;
-	}
-
-	contadorChaoPosition = 0;
-
-	for(int i = 142; i < 144; i++){
-		plataforma[i].setSize(sf::Vector2f(30,15));
-		plataforma[i].setPosition(255 - contadorChaoPosition, 115);
-		contadorChaoPosition += 30;
-	}
-
-	contadorChaoPosition = 0;
-}*/
+#include "player.hpp"
 
 void createPlatforms(sf::RectangleShape *plataforma){
 	int contadorChaoPosition = 0;
@@ -248,30 +141,19 @@ int main(int argc, char **argv) {
 		plataforma[i].setTexture(&platformTexture);
 	}
 
-	sf::Texture marioTexture;
-	sf::Sprite marioSprite;
-
-	int velMarioX = 0, velMarioY = 0;
-
 	sf::Image icon = sf::Image { };
 	if(!icon.loadFromFile("assets/iconPage.png")){
 		return -1;
 	}
+
 	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
-	if(!marioTexture.loadFromFile("assets/mario.png")){
-		return -1;
-	}
-	marioSprite.setTexture(marioTexture);
-
-	marioSprite.setOrigin(marioSprite.getLocalBounds().width/2., marioSprite.getLocalBounds().height/2.);
-	marioSprite.setPosition(130,551);
-	marioSprite.setScale(0.065, 0.065);
+	Player player;
 
 	while(window.isOpen()){
 		sf::Event event;
-		velMarioX = 0;
-		velMarioY = 0;
+
+		player.setVelToZero();
 
 		while(window.pollEvent(event)){
 			if(event.type == sf::Event::Closed){
@@ -279,22 +161,10 @@ int main(int argc, char **argv) {
 			}
 		}
 
-		if((sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) && (marioSprite.getPosition().x >= marioSprite.getTexture()->getSize().x * marioSprite.getScale().x / 5)){
-			velMarioX = -5;
-		}
-		if((sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) && (marioSprite.getPosition().x <= 800 - marioSprite.getTexture()->getSize().x * marioSprite.getScale().x / 4.1)){
-			velMarioX = 5;
-		}
-
-		marioSprite.setPosition(marioSprite.getPosition().x + velMarioX, marioSprite.getPosition().y + velMarioY);
+		player.move();
 
 		window.clear(sf::Color::Black);
-		window.draw(marioSprite);
-		window.draw(marioSprite);
-		/*while(NumPlataforma<8){
-			window.draw(plataforma[NumPlataforma]);
-			NumPlataforma++;
-		};*/
+		player.draw(window);
 
 		for(NumPlataforma = 0; NumPlataforma<141;NumPlataforma++){
 			window.draw(plataforma[NumPlataforma]);
---
+++ DonkeyKong/player.hpp
@@ -0,0 +1,65 @@
+/*
+ * player.hpp
+ *
+ *  Created on: 27 de ago. de 2024
+ *      Author: Revii
+ */
+
+#ifndef SOURCE_PLAYER_HPP_
+#define SOURCE_PLAYER_HPP_
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+class Player{
+private:
+	sf::Texture texturePlayer;
+	sf::Sprite playerSprite;
+	int velX = 0, velY = 0;
+public:
+	bool isPlayerCreated;
+
+	Player(){
+		if(!texturePlayer.loadFromFile("assets/mario.png")){
+			isPlayerCreated = false;
+		}
+		playerSprite.setTexture(texturePlayer);
+
+		playerSprite.setOrigin(playerSprite.getLocalBounds().width/2., playerSprite.getLocalBounds().height/2.);
+		playerSprite.setPosition(130,551);
+		playerSprite.setScale(0.065, 0.065);
+	}
+
+	int getVelX(){
+		return velX;
+	}
+
+	int getVelY(){
+		return velY;
+	}
+
+	void setVelToZero(){
+		velX = velY = 0;
+	}
+
+	void move(){
+		if((sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) && (playerSprite.getPosition().x >= playerSprite.getTexture()->getSize().x * playerSprite.getScale().x / 5)){
+			velX = -5;
+		}
+		if((sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) && (playerSprite.getPosition().x <= 800 - playerSprite.getTexture()->getSize().x * playerSprite.getScale().x / 4.1)){
+			velX = 5;
+		}
+
+		playerSprite.setPosition(playerSprite.getPosition().x + velX, playerSprite.getPosition().y + velY);
+	}
+
+	void draw(sf::RenderWindow &window){
+		window.draw(playerSprite);
+	}
+
+};
+
+#endif /* SOURCE_PLAYER_HPP_ */
